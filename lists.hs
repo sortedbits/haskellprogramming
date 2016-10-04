0072c018a8e57b16cb8ed03c5c7217d9cf765e63
sortedbits <sricox@gmail.com>
2016-10-04 07:49:39 +0200

    init

--- lists.hs
+++ lists.hs
@@ -205,6 +205,19 @@ noArticles :: [Char] -> [[Char]]
 noArticles = filter (\x -> x /= "a" && x /= "an") . words
 
 
--- Ciphers
+-- Zipping
 
--- TODO
+myZip :: [a] -> [b] -> [(a, b)]
+myZip [] _ = []
+myZip _ [] = []
+myZip (x : xs) (y : ys) = (x, y) : myZip xs ys 
+
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith f [] _ = []
+myZipWith f _ [] = []
+myZipWith f (x : xs) (y : ys) = f x y : myZipWith f xs ys
+
+myZip' :: [a] -> [b] -> [(a, b)]
+myZip' = myZipWith (\x y -> (x, y))
+
+
